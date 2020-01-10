1bf7211c62fd5e2639c6d022da082eaedc33e669
Vasyl Krupka <krupka.ua@gmail.com>
2020-01-10 03:53:51 +0200

    fixed bugs when user adding new tasks to the form

--- todo-task/js/script.js
+++ todo-task/js/script.js
@@ -1,4 +1,5 @@
-const tasks = [{
+const tasks = [
+  {
     _id: '5d2ca9e2e03d40b326596aa7',
     completed: true,
     body: 'Occaecat non ea quis occaecat ad culpa amet deserunt incididunt elit fugiat pariatur. Exercitation commodo culpa in veniam proident laboris in. Excepteur cupidatat eiusmod dolor consectetur exercitation nulla aliqua veniam fugiat irure mollit. Eu dolor dolor excepteur pariatur aute do do ut pariatur consequat reprehenderit deserunt.\r\n',
@@ -20,7 +21,8 @@ const tasks = [{
     _id: '5d2ca9e29c8a94095564788e0',
     completed: false,
     body: 'Aliquip cupidatat ex adipisicing veniam do tempor. Lorem nulla adipisicing et esse cupidatat qui deserunt in fugiat duis est qui. Est adipisicing ipsum qui cupidatat exercitation. Cupidatat aliqua deserunt id deserunt excepteur nostrud culpa eu voluptate excepteur. Cillum officia proident anim aliquip. Dolore veniam qui reprehenderit voluptate non id anim.\r\n',
-    title: 'Deserunt laborum id consectetur pariatur veniam occaecat occaecat tempor voluptate pariatur nulla reprehenderit ipsum.',
+    title: 
+    'Deserunt laborum id consectetur pariatur veniam occaecat occaecat tempor voluptate pariatur nulla reprehenderit ipsum.',
   },
 ];
 
@@ -32,7 +34,7 @@ const tasks = [{
   const objOfTasks = arrOfTasks.reduce((acc, task) => {
     acc[task._id] = task;
     return acc;
-  }, {})
+  }, {});
 
   // Element UI
   const listContainer = document.querySelector('.tasks-list-section .list-group',
@@ -46,7 +48,7 @@ const tasks = [{
 
   // Events
   renderAllTasks(objOfTasks);
-  form.addEventListener('Submit', onFormSubmitHandler)
+  form.addEventListener('submit', onFormSubmitHandler)
 
 
   /**
@@ -98,7 +100,6 @@ const tasks = [{
     e.preventDefault();
     const titleValue = inputTitle.value;
     const bodyValue = inputBody.value;
-    // console.log(titleValue, bodyValue);
 
     if (!titleValue || !bodyValue) {
       alert("Пожалуйста введите title и body");
@@ -106,7 +107,10 @@ const tasks = [{
     }
 
     const task = createNewTask(titleValue, bodyValue);
-    console.log(objOfTasks);
+    const listItem = listItemTemplate(task);
+    // Add new task in DOM tree
+    listContainer.insertAdjacentElement("afterbegin", listItem);
+    form.reset();
   }
 
   function createNewTask(title, body) {
@@ -116,7 +120,6 @@ const tasks = [{
       completed: false,
       _id: `task-${Math.random()}`,
     };
-    console.log(newTask);
 
     objOfTasks[newTask._id] = newTask;
 
