0165848ed999b73bfb5f7d79d897f32d79d504aa
Vasyl Krupka <krupka.ua@gmail.com>
2020-01-16 04:08:03 +0200

    Ajax POST request

--- Ajax/js/script.js
+++ Ajax/js/script.js
@@ -9,10 +9,13 @@
  * XMLHttpRequest возвращает нам на выходе объект, который и позволяет на создавать, отправлять, обрабатывать запросы.
  */
 
-const btn = document.querySelector('button');
+const btnGetPosts = document.querySelector('.btn-get-posts');
+const btnAddPost = document.querySelector('.btn-add-post');
 const container = document.querySelector('.container');
 
 
+
+ // ============= GET =============
  function getPosts(cb) {
 
   const xhr = new XMLHttpRequest();
@@ -37,31 +40,70 @@ const container = document.querySelector('.container');
    xhr.send();
  }
 
+
+ // ============= POST =============
+ function createPost(body, cb) {
+  const xhr = new XMLHttpRequest();
+   xhr.open("POST", "https://jsonplaceholder.typicode.com/posts");
+   xhr.addEventListener("load", () => {
+     const response = JSON.parse(xhr.responseText)
+     cb(response);
+   });
+
+   xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+  
+   xhr.addEventListener("error", () => {
+     console.log("error");
+   });
+  
+   xhr.send(JSON.stringify(body));
+ }
+
+ function cardTemplate(post) {
+  const card = document.createElement('div');
+  card.classList.add('card');
+  const cardBody = document.createElement('div');
+  cardBody.classList.add('card-body');
+  const title = document.createElement('h5');
+  title.classList.add('card-title');
+  title.textContent = post.title;
+  const article = document.createElement('p');
+  article.classList.add('card-text');
+  article.textContent = post.body;
+  cardBody.appendChild(title);
+  cardBody.appendChild(article);
+  card.appendChild(cardBody);
+  return card;
+ }
+
+
+
  function renderPosts(response) {
   const fragment = document.createDocumentFragment();
   response.forEach(post => {
-    const card = document.createElement('div');
-    card.classList.add('card');
-    const cardBody = document.createElement('div');
-    cardBody.classList.add('card-body');
-    const title = document.createElement('h5');
-    title.classList.add('card-title');
-    title.textContent = post.title;
-    const article = document.createElement('p');
-    article.classList.add('card-text');
-    article.textContent = post.body;
-    cardBody.appendChild(title);
-    cardBody.appendChild(article);
-    card.appendChild(cardBody);
+    const card = cardTemplate(post);
     fragment.appendChild(card);
-    // console.log(cardBody)
   });
   container.appendChild(fragment);
  }
 
- btn.addEventListener('click', e => {
+ btnGetPosts.addEventListener('click', e => {
     // вызываем функцию и передаем в нее response
   getPosts(renderPosts);
+});
+
+
+
+btnAddPost.addEventListener('click', e => {
+  const newPost = {
+    title: 'foo',
+    body: 'bar',
+    userId: 1
+  };
+  createPost(newPost, response => {
+    const card = cardTemplate(response);
+    container.insertAdjacentElement('afterbegin', card);
+  });
 });
 
 
