9d930d094e51fa5e3bf569c0dbf6f0832545b267
Vasyl Krupka <krupka.ua@gmail.com>
2020-01-17 11:12:42 +0200

    Promise: resolve & reject

--- Promise/js/app.js
+++ Promise/js/app.js
@@ -3,8 +3,41 @@
 
 // === <<<--- SOME TEXT  --->>>  ===
 
-const Promise = new Promes((resolve, reject) => {
-  setTimeout(() => resolve(Math.random()), 10000);
+/**
+ * promise - позволяет организовать синхронный код таким образом, что он будет выполняться в некой последовательности, друг за другом, и гарантировано можно передавать результаты между цепочками, при этом есть уверенность что resolve или reject будет вызван лишь 1 раз и эти значение будут получены в цепочки than.
+ * 
+ * Ошибки можно отлавливать в методе catch. Метод catch в цепочке вызовов становиться самым последним.
+ * Если, любой из than в цепочке вызовов выдаст ошибку - эта ошибка будет поймана в метод catch
+ */
+
+ // ======= Promise resolve =======
+// const promise = new Promise((resolve, reject) => {
+//   setTimeout(() => resolve(Math.random()), 1000);
+// });
+
+// console.log(promise);
+// promise
+//   .then(x => {
+//     console.log(x);
+//     return x;
+//   })
+// .then(y => console.log(y))
+// .catch(err => console.log(err));
+
+// promise.then(z => console.log(z));
+
+
+
+ // ======= Promise reject =======
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => reject('error'), 1000);
 });
 
-console.log(Promise);
+console.log(promise2);
+promise2
+  .then(x => {
+    console.log(x);
+    return x;
+  })
+.then(y => console.log(y))
+.catch(err => console.log(err));
