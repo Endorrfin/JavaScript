0a6d53dce988214bea6a3e641a7e959287c43353
Vasyl Krupka <krupka.ua@gmail.com>
2020-01-17 13:47:34 +0200

    some examples work with async await

--- async await/js/app.js
+++ async await/js/app.js
@@ -1,4 +1,3 @@
-
 // 'use strict';
 
 // === <<<--- SOME TEXT  --->>>  ===
@@ -20,10 +19,78 @@
 //   });
 // }
 
+// ======= async await I =======
+// async function getPost(id) {
+//   const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`,
+//   );
+//   console.log(response);
+// }
 
+
+// getPost(1);
+
+
+
+
+// ======= async await II =======
+// async function getPost(id) {
+//   const response = await fetch(
+//      `https://jsonplaceholder.typicode.com/posts/${id}`,
+//   );
+  
+//   const data = await response.json();
+//   return data;
+// }
+
+// getPost(1)
+//   .then(data => console.log(data))
+//   .catch(err => console.log(err))
+
+
+
+
+  // ======= async await III =======
+// async function getPost(id) {
+//   try {
+//     const response1 = await fetch(
+//       `https://jsonplaceholder.typicode.com/posts/${id}`,
+//     ).then(res => res.json());
+//     const response2 = await fetch(
+//       `https://jsonplaceholder.typicode.com/users`,
+//     ).then(res => res.json());
+
+//     return [response1, response2];
+//   } catch (err) {
+//     console.log(err);
+//     return Promise.reject(err);
+//   }
+// }
+
+// getPost(1)
+//   .then(data => console.log(data))
+//   .catch(err => console.log(err))
+
+
+
+
+
+    // ======= async await & prmisAll =======
 async function getPost(id) {
-  return 'someTest';
+  try {
+    const response = await fetch(
+      `https://jsonplaceholder.typicode.com/posts/${id}`,
+    ).then(res => res.json());
+
+    return response;
+  } catch (err) {
+    console.log(err);
+    return Promise.reject(err);
+  }
 }
 
+async function getAll() {
+  const [res1, res2] = await Promise.all([getPost(1), getPost(2)]);
+  console.log(res1, res2);
+}
 
-
+getAll ();
