8a86d41c70d691fd20dd1f8eecbeee174dc99493
Vasyl Krupka <krupka.ua@gmail.com>
2020-01-17 10:27:53 +0200

    how work Promise

---
+++ Promise/js/app.js
@@ -0,0 +1,10 @@
+
+// 'use strict';
+
+// === <<<--- SOME TEXT  --->>>  ===
+
+const Promise = new Promes((resolve, reject) => {
+  setTimeout(() => resolve(Math.random()), 10000);
+});
+
+console.log(Promise);
