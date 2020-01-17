bbbc896dcf504b1d031b1eb1ae99a0a58b75130f
Vasyl Krupka <krupka.ua@gmail.com>
2020-01-17 16:08:43 +0200

    async await - principle of operation

--- async await/js/app.js
+++ async await/js/app.js
@@ -6,8 +6,37 @@
  * async await - это новый формат написания асинхронного кода.
  * Когда мы объявляем функцию с ключевым словом async, то async превращает эту функцию в такую, которая всегда будет возвращать promise.
  * 
+ * Ключевое слово async перед функцией гарантирует, что эта функция в любом случае вернёт промис.
+ * 
+ * 
+ * Ключевое слово await заставит интерпретатор JavaScript ждать до тех пор, пока промис справа от await не выполнится.
  * Внутри функции мы можем использовать ключевое слово await, которое как бы замораживает наш код до выполнения асинхронного действия.
  */
+
+
+
+ // ======= code I =======
+// async function f() {
+//   return 1;
+// }
+
+// f().then(console.log); // 1
+
+
+ // ======= code II =======
+async function f() {
+
+  let promise = new Promise((resolve, reject) => {
+    setTimeout(() => resolve("готово!"), 1000)
+  });
+
+  let result = await promise; // будет ждать, пока промис не выполнится (*)
+
+  console.log(result); // "готово!"
+}
+
+f();
+
 
 // ======= async await - option I =======
 // function getPosts(id) {
@@ -75,22 +104,22 @@
 
 
     // ======= async await & prmisAll =======
-async function getPost(id) {
-  try {
-    const response = await fetch(
-      `https://jsonplaceholder.typicode.com/posts/${id}`,
-    ).then(res => res.json());
+// async function getPost(id) {
+//   try {
+//     const response = await fetch(
+//       `https://jsonplaceholder.typicode.com/posts/${id}`,
+//     ).then(res => res.json());
 
-    return response;
-  } catch (err) {
-    console.log(err);
-    return Promise.reject(err);
-  }
-}
+//     return response;
+//   } catch (err) {
+//     console.log(err);
+//     return Promise.reject(err);
+//   }
+// }
 
-async function getAll() {
-  const [res1, res2] = await Promise.all([getPost(1), getPost(2)]);
-  console.log(res1, res2);
-}
+// async function getAll() {
+//   const [res1, res2] = await Promise.all([getPost(1), getPost(2)]);
+//   console.log(res1, res2);
+// }
 
-getAll ();
+// getAll ();
