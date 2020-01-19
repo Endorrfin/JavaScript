45c31b62440420aa8fddd6ce348eb1c6a29999a3
Vasyl Krupka <krupka.ua@gmail.com>
2020-01-19 12:09:23 +0200

    JS: classes in ES-6

--- es6-classes/js/app.js
+++ es6-classes/js/app.js
@@ -1,91 +1,23 @@
 
 // 'use strict';
 
- /**
-  * Object.create - это специальный метод, который позволяет создать новый объект с указанным объектом прототипа и свойствами.
-  * 
-  * В JS различают 2 наследования:
-  * Функциональное наследование
-  * Прототипное наследование
-  * 
-  * Метод call - дает возможность вызвать фукнцию с заданным контекстом
-  */
-
-  const protoForObj = {
-    sayHello() {
-      return "Hello world"
-    }
-  }
-
-  const obj = Object.create(protoForObj, {
-    firstName: {
-      value: 'Bill',
-    },
-  });
-  console.log(obj); // Object   __proto__:   sayHello: ƒ sayHello()   __proto__: Object
-  console.log(obj.sayHello()); // Hello world
-
-  const user = new User('Bill', 'Gates');
-
-
-
-// ======= create class User =======
-function User (firstName, lastName) {
-  this.firstName = firstName;
-  this.lastName = lastName;
-}
-
-User.prototype.getFulName = function () {
-  return `${this.firstName} ${this.lastName}`;
-};
-
-User.prototype.seyHello = function () {
-  return `Hello ${this.firstName} ${this.lastName}`;
-};
-
-console.log(user); // User {firstName: "Bill", lastName: "Gates"}
-console.log(user.getFulName()); // Bill Gates
-console.log(user.seyHello()); // Hello Bill Gates
-
-
-
-// ======= create class Customer --> extends User (functional inheritance) =======
-function Customer (firstName, lastName, membership) {
-  // User.call(this, firstName, lastName) // привязываем контекст с помощью метода apply
-  User.apply(this, arguments); // если параметров много, можем применить apply и передать arguments
-
-  // сохраняем membership
-  this.membership = membership;
-}
-
-
-const customer = new Customer ('Elon', 'Musk', 'basic');
-console.log(customer); // Customer {firstName: "Elon", lastName: "Musk"}
-console.log(customer.sayHello); // UNDEFINED
-
-
-// прототипное наследование: забираем прототип user в прототип customer
-Customer.prototype = Object.create(User.prototype);
-Customer.prototype.constructor = Customer;
-Customer.prototype.getMemberShip = function () {
-  return this.membership.toUpperCase();
-}
-
-console.log(customer); // Customer {firstName: "Elon", lastName: "Musk"}
-console.log(customer.getFulName); // undefined
-console.log(customer.sayHello); // undefined
-
-// console.log(customer.getMemberShip());
-
-
 
 // +++++++ ES-6 +++++++
 class ProductES {
   constructor(brand, price, discount) {
-    this.brand = brand;
+    this._brand = brand;
     this.price = price;
     this.discount = discount;
   }
+
+  get brand() {
+    return this._brand;
+  }
+
+  set brand(name) {
+    this._brand = name;
+  }
+
   getPriceWithDiscount() {
     return (this.price * (100 - this.discount)) / 100;
   }
@@ -93,6 +25,7 @@ class ProductES {
   setPrice (newPrice) {
     this.price = newPrice
   }
+  
 
   /**
    * Классы ES-6 дают возможность создавать статические методы. 
@@ -110,6 +43,16 @@ const newProduct = new ProductES('Bosch', 200, 10);
 console.log(newProduct);
 console.log(newProduct.getPriceWithDiscount()) //180
 
+console.log(ProductES.plus(3,4)); // 7
+
+/**
+ * class Date - который дает возможность работать с датой
+ * Это специальная функция-конструктор, которая дает возможность работать с датой.
+ * В нем есть множество методов для получения года, месяца, для установки даты и т.д.
+ * в class Date есть статический метода - называется null - он возвращает time stamp - момент времени.
+ * Это число в милисекундах - сколько прошло времени от 1970 года 1-го января 00 часов 00 минут 00 секунд до сегодняшнего дня.
+ * time stamp - позволяет решить погрешность обусловленную часовыми поясами.
+ */
 
 
 
